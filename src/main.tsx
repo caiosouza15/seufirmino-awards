b885c3f8f3e0abd4da6cea53c98a7518278375f2
caiosouza15 <caiosouza.15@outlook.com>
2025-12-10 11:11:23 -0300

    mudanças para deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/globals.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter basename="/seufirmino-awards-/">
+    <BrowserRouter basename="/seufirmino-awards/">
       <App />
     </BrowserRouter>
   </StrictMode>
